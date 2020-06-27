b473f3f85bc901647acf1e516713867656203901
PeterStanger92020362 <psta191@mywhitecliffe.com>
2020-06-27 12:50:43 +1200

    completed all tasks.

--- src/JavaBuiltInSort.java
+++ src/JavaBuiltInSort.java
@@ -14,10 +14,22 @@ public class JavaBuiltInSort {
         System.out.println();
     }
 
+    void printArray(String arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.println(arr[i]);
+        System.out.println();
+        System.out.println();
+    }
+
     // Driver method to test above
     public static void main(String args[])
     {
         JavaBuiltInSort ob = new JavaBuiltInSort();
+
+        /*
+
         Integer arr[] = {64, 34, 25, 12, 22, 90, 11};
         System.out.println("The Unsorted array is");
         ob.printArray(arr);
@@ -32,6 +44,35 @@ public class JavaBuiltInSort {
 
         sort(arr, Collections.reverseOrder());
         System.out.println("The Java Built in Reverse Sorted array is");
+        ob.printArray(arr);
+
+         */
+
+        String arr[] = {
+                "Michael",
+                "Tamara",
+                "Mackenzie",
+                "Caius",
+                "Emilia",
+                "Katie",
+                "Michael",
+                "Maggie",
+                "Austin"
+        };
+
+        System.out.println("The Unsorted array is");
+        ob.printArray(arr);
+
+        sort(arr);
+        System.out.println("The Java Built In Sorted array is");
+        ob.printArray(arr);
+
+        sort(arr, Collections.reverseOrder());
+        System.out.println("The Java Built in Reverse Sorted array is");
+        ob.printArray(arr);
+
+        sort(arr, arr.length-4, arr.length);
+        System.out.println("The Java Built in Sort of the last 4 elements is");
         ob.printArray(arr);
     }
 
